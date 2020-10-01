630a51e51e9cb3ab03aca82714585ecae75de0a2
Jessica Munoz <ms.jessica.munoz@gmail.com>
2020-10-01 13:49:17 -0700

    It will only include the starter folder and include string and variable examples

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,15 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = "Jess"
+age = 31
+
+# Concatenate
+# print("Hello, my name is + " + name + " and I am " + str(age))
 
 # String Formatting
+
+# Args by position
+
+print("Hello, my name is {name} and I'm {age}".format(name=name, age=age))
 
 # String Methods
